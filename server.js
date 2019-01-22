ade2ae610a85e7b955028d1058725cdf2faa4184
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-01-22 19:18:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,31 +2,50 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/",(req,res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use("/:dateVal",(req,res) => {
+    try{
+      decodeURIComponent(req.params.dateVal)
+    }catch(error){
+      console.log(error)
+      res.send("Bad Request")
+    }
+    var param = decodeURIComponent(req.params.dateVal);
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    var dateOptions = {month : 'long' , year: 'numeric' , day: 'numeric'};
+    //treating natural date input
+    if(isNaN(param)){
+      if(String(new Date(param)) === "Invalid Date"){
+        res.json({unix:null, natural:null}); 
+      }
+      var naturalDate = new Date(param);
+      naturalDate = naturalDate.toLocaleDateString('en-US', dateOptions);
+      var unixDate = new Date(param).getTime()/1000;
+    }
+    //treating unix input
+    else{
+      var unixDate = Number(param)
+      var naturalDate = new Date(unixDate*1000);
+      naturalDate = naturalDate.toLocaleDateString('en-US', dateOptions);
+    }
+    res.json({unix:unixDate, natural:naturalDate}); 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
+   
+})
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const  listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+
+})
