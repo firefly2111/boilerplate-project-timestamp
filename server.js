e19b0b64dbcb01e2c749b704718a2ff7b92a6de0
Nikola Milicevic <nikolamilicevic99@yahoo.com>
2019-03-26 22:28:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var cors = require("cors");
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
 var app = express();
-app.use(bodyParser.json());
-app.use(cors());
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/views/index.html");
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/api/timestamp/:date_string", function(req, res){
-  var dateVal = req.params.date_string;
-  var dateFormat = {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric"
-  };
-  if(isNaN(dateVal)){
-       var naturalDate = new Date(dateVal);
-      naturalDate = naturalDate.toLocaleDateString("en-us", dateFormat);
-      var unixDate = new Date(dateVal).getTime()/1000;
-     }else{
-       var unixDate = dateVal;
-       var naturalDate = new Date(dateVal * 1000);
-       naturalDate = naturalDate.toLocaleDateString("en-us", dateFormat);
-     }
-  res.json({unix: unixDate, utc: naturalDate});
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let setDate = req.params.date_string;
+  let utcTime = new Date(setDate);
+  let unixTime = Math.round(utcTime / 1000);
+  res.json({"utc": utcTime, unix: unixTime});
 });
-app.listen(3000, function(){
-  console.log("Working");
+
+app.get("/api/timestamp", function(req, res){
+  let utcTime = new Date();
+  let unixTime = Math.round(utcTime / 1000);
+  
+  res.json({"utc": utcTime, "unix": unixTime});
 });
-module.exports = app;
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
